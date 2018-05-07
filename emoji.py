263467b6be8a8b42b379143aff754134957db66e
Kotaiba Alachkar <36328185+kalachkar@users.noreply.github.com>
2018-05-07 11:25:33 +0200

    List of used emoji

---
+++ emoji.py
@@ -0,0 +1,110 @@
+Smileys & People 1-84 inclusive:
+
+Grinning face
+Grimacing face
+Grinning face with smiling eyes
+Face with tears of joy
+Smiling face with open mouth
+Smiling face with open mouth and smiling eyes
+Rolling on the floor laughing
+Smiling face with open mouth and cold sweat
+Smiling face with open mouth and tightly-closed eyes
+Smiling face with halo
+Winking face
+Smiling face with smiling eyes
+Slightly smiling face
+Upside-down face
+Smiling face
+Face savouring delicious food
+Relieved face
+Smiling face with heart-shaped eyes
+Face throwing a kiss
+Kissing face
+Kissing face with smiling eyes
+Kissing face with closed eyes
+Crazy face
+Face with stuck-out tongue and winking eye
+Face with stuck-out tongue and tightly-closed eyes
+Face with stuck-out tongue
+Money-mouth face
+Smiling face with sunglasses
+Nerd face
+Face with monocle
+Face with cowboy hat
+Hugging face
+Clown face
+Smirking face
+Face without mouth
+Neutral face
+Expressionless face
+Unamused face
+Face with rolling eyes
+Face with raised eyebrow
+Thinking face
+Shushing face
+Face with hand over mouth
+Lying face
+Flushed face
+Disappointed face
+Worried face
+Angry face
+Pouting face
+Face with symbols over mouth
+Pensive face
+Confused face
+Slightly frowning face
+Frowning face
+Persevering face
+Confounded face
+Tired face
+Weary face
+Face with look of triumph
+Face with open mouth
+Face screaming in fear
+Fearful face
+Face with open mouth and cold sweat
+Hushed face
+Frowning face with open mouth
+Anguished face
+Crying face
+Disappointed but relieved face
+Sleepy face
+Drooling face
+Face with cold sweat
+Loudly crying face
+Star-struck
+Dizzy face
+Astonished face
+Exploding head
+Zipper-mouth face
+Face with medical mask
+Face with head-bandage
+Face with thermometer
+Face vomiting
+Nauseated face
+Sneezing face
+Sleeping face
+
+Smileys & People 86, 87, 92, 93, 94, 97-105 inclusive:
+
+Smiling face with horns
+Imp
+Skull
+Skull and crossbones
+Extraterrestrial alien
+Smiling cat face with open mouth
+Grinning cat face with smiling eyes
+Cat face with tears of joy
+Smiling cat face with heart-shaped eyes
+Cat face with wry smile
+Kissing cat face with closed eyes
+Weary cat face
+Crying cat face
+Pouting cat face
+Animals & nature 152, 153:
+Fire
+Collision symbol
+
+Symbols 1:
+
+Heavy red heart
