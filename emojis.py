15fe58e6db23a3dfb217c4cd9e236ea3c709bf41
Kotaiba Alachkar <36328185+kalachkar@users.noreply.github.com>
2018-05-11 17:27:57 +0200

    fix the emojis dictionary

---
+++ emojis.py
@@ -0,0 +1,112 @@
+# An emojis dictionary that contains key : value pairs (emoji name: numeric value):
+emojisDic = {
+		#Smileys & People 1-84 inclusive:
+		"<Emoji: Grinning face>" : 1,  
+		"<Emoji: Grimacing face>" : 2,
+		"<Emoji: Grinning face with smiling eyes>" : 3,
+		"<Emoji: Face with tears of joy>" : 4,
+		"<Emoji: Smiling face with open mouth>" : 5,
+		"<Emoji: Smiling face with open mouth and smiling eyes>" : 6,
+		"<Emoji: Rolling on the floor laughing>" : 7,
+		"<Emoji: Smiling face with open mouth and cold sweat>" : 8,
+		"<Emoji: Smiling face with open mouth and tightly-closed eyes>" : 9,
+		"<Emoji: Smiling face with halo>" : 10,
+		"<Emoji: Winking face>" : 11,
+		"<Emoji: Smiling face with smiling eyes>" : 12,
+		"<Emoji: Slightly smiling face>" : 13,
+		"<Emoji: Upside-down face>" : 14,
+		"<Emoji: Smiling face>" : 15,
+		"<Emoji: Face savouring delicious food>" : 16,
+		"<Emoji: Relieved face>" : 17,
+		"<Emoji: Smiling face with heart-shaped eyes>" : 18,
+		"<Emoji: Face throwing a kiss>" : 19,
+		"<Emoji: Kissing face>" : 20,
+		"<Emoji: Kissing face with smiling eyes>" : 21,
+		"<Emoji: Kissing face with closed eyes>" : 22,
+		"<Emoji: Crazy face>" : 23,
+		"<Emoji: Face with stuck-out tongue and winking eye>" : 24,
+		"<Emoji: Face with stuck-out tongue and tightly-closed eyes>" : 25,
+		"<Emoji: Face with stuck-out tongue>" : 26,
+		"<Emoji: Money-mouth face>" : 27,
+		"<Emoji: Smiling face with sunglasses>" : 28,
+		"<Emoji: Nerd face>" : 29,
+		"<Emoji: Face with monocle>" : 30,
+		"<Emoji: Face with cowboy hat>" : 31,
+		"<Emoji: Hugging face>" : 32,
+		"<Emoji: Clown face>" : 33,
+		"<Emoji: Smirking face>" : 34,
+		"<Emoji: Face without mouth>" : 35,
+		"<Emoji: Neutral face>" : 36,
+		"<Emoji: Expressionless face>" : 37,
+		"<Emoji: Unamused face>" : 38,
+		"<Emoji: Face with rolling eyes>" : 39,
+		"<Emoji: Face with raised eyebrow>" : 40,
+		"<Emoji: Thinking face>" : 41,
+		"<Emoji: Shushing face>" : 42,
+		"<Emoji: Face with hand over mouth>" : 43,
+		"<Emoji: Lying face>" : 44,
+		"<Emoji: Flushed face>" : 45,
+		"<Emoji: Disappointed face>" : 46,
+		"<Emoji: Worried face>" : 47,
+		"<Emoji: Angry face>" : 48,
+		"<Emoji: Pouting face>" : 49,
+		"<Emoji: Face with symbols over mouth>" : 50,
+		"<Emoji: Pensive face>" : 51,
+		"<Emoji: Confused face>" : 52,
+		"<Emoji: Slightly frowning face>" : 53,
+		"<Emoji: Frowning face>" : 54,
+		"<Emoji: Persevering face>" : 55,
+		"<Emoji: Confounded face>" : 56,
+		"<Emoji: Tired face>" : 57,
+		"<Emoji: Weary face>" : 58,
+		"<Emoji: Face with look of triumph>" : 59,
+		"<Emoji: Face with open mouth>" : 60,
+		"<Emoji: Face screaming in fear>" : 61,
+		"<Emoji: Fearful face>" : 62,
+		"<Emoji: Face with open mouth and cold sweat>" : 63,
+		"<Emoji: Hushed face>" : 64,
+		"<Emoji: Frowning face with open mouth>" : 65,
+		"<Emoji: Anguished face>" : 66,
+		"<Emoji: Crying face>" : 67,
+		"<Emoji: Disappointed but relieved face>" : 68,
+		"<Emoji: Sleepy face>" : 69,
+		"<Emoji: Drooling face>" : 70,
+		"<Emoji: Face with cold sweat>" : 71,
+		"<Emoji: Loudly crying face>" : 72,
+		"<Emoji: Star-struck>" : 73,
+		"<Emoji: Dizzy face>" : 74,
+		"<Emoji: Astonished face>" : 75,
+		"<Emoji: Exploding head>" : 76,
+		"<Emoji: Zipper-mouth face>" : 77,
+		"<Emoji: Face with medical mask>" : 78,
+		"<Emoji: Face with head-bandage>" : 79,
+		"<Emoji: Face with thermometer>" : 80,
+		"<Emoji: Face vomiting>" : 81,
+		"<Emoji: Nauseated face>" : 82,
+		"<Emoji: Sneezing face>" : 83,
+		"<Emoji: Sleeping face>" : 84,
+		
+		#Smileys & People 86, 87, 92-105 inclusive:
+		"<Emoji: Smiling face with horns>" : 85, #Smileys & People 86 
+		"<Emoji: Imp>" : 86, #Smileys & People 87
+		"<Emoji: Skull>" : 87, #Smileys & People 92
+		"<Emoji: Skull and crossbones>" : 88, #Smileys & People 93
+		"<Emoji: Extraterrestrial alien>" : 89, #Smileys & People 94
+		"<Emoji: Smiling cat face with open mouth>" : 90, #Smileys & People 97
+		"<Emoji: Grinning cat face with smiling eyes>" : 91, #Smileys & People 98
+		"<Emoji: Cat face with tears of joy>" : 92, #Smileys & People 99
+		"<Emoji: Smiling cat face with heart-shaped eyes>" : 93, #Smileys & People 100
+		"<Emoji: Cat face with wry smile>" : 94, #Smileys & People 101
+		"<Emoji: Kissing cat face with closed eyes>" : 95, #Smileys & People 102
+		"<Emoji: Weary cat face>" : 96, #Smileys & People 103
+		"<Emoji: Crying cat face>" : 97, #Smileys & People 104
+		"<Emoji: Pouting cat face>" : 98, #Smileys & People 105
+		
+		#Animals & nature 152, 153:
+		"<Emoji: Fire>" : 99, #Animals & nature 152
+		"<Emoji: Collision symbol>" : 100, #Animals & nature 153
+		
+		#Symbols 1:
+		"<Emoji: Heavy red heart>" : 101, #Symbols 1
+}
+
