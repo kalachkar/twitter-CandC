17cff5524e3f170bdb3e385984076bc622317e99
Kotaiba Alachkar <36328185+kalachkar@users.noreply.github.com>
2018-05-12 12:16:35 +0200

    Finalizing the basic functionality

--- daemon.py
+++ daemon.py
@@ -1,12 +1,21 @@
-import tweepy, twitter, json, twint, requests, re, time, os, string
+import tweepy
+import twitter
+import json
+import twint
+import requests
+import re
+import time
+import os
+import string
 from credentials import *
 from emojis import emojisDic
 from bs4 import BeautifulSoup
 
-#Get Today's date
+# Get Today's date
 TODAY = time.strftime("%Y-%m-%d")
 
-#check whether the string can be encoded only with ASCII characters
+
+# check whether the string can be encoded only with ASCII characters
 def isEnglish(t):
     try:
         t.encode(encoding='utf-8').decode('ascii')
@@ -15,100 +24,113 @@ def isEnglish(t):
     else:
         return True
 
-#Check whether its a command of not by checking if there is 3 space in the tweet
+# Check whether its a command of not by checking if there is 3 space in
+# the tweet
+
+
 def isCommand(tweet):
-	return True if tweet.count('   ') > 0 else False
+    return True if tweet.count('   ') > 0 else False
+
+# Remove the additional zeros' while constructing the ip address
+
 
 def zeroRemover(x):
-	l = x.split('.')
-	
-	for i in range(0, len(l)):
-		if len(l[i]) == 4:
-			l[i] = l[i].replace('00', '0', 1)
-		elif len(l[i])== 5:
-			l[i] = l[i].replace('00', '', 1)
-	
-	ip = '.'.join(l)	
-	
-	return ip
+    l = x.split('.')
 
-#get a clear IP address from a tweet's emojis
+    for i in range(0, len(l)):
+        if len(l[i]) == 4:
+            l[i] = l[i].replace('00', '0', 1)
+        elif len(l[i]) == 5:
+            l[i] = l[i].replace('00', '', 1)
+    ip = '.'.join(l)
+
+    return ip
+
+# get a clear IP address from a tweet's emojis
+
+
 def toIP(z):
-	lss = re.findall('<.*>', z)
-	emojiIp = str(lss[0]).replace('> ', '>.')
-	#print(emojiIp)
-	for key, value in emojisDic.items():
-		emojiIp = emojiIp.replace(key, str(value))
+    lss = re.findall('<.*>', z)
+    emojiIp = str(lss[0]).replace('> ', '>.')
+    # print(emojiIp)
+    for key, value in emojisDic.items():
+        emojiIp = emojiIp.replace(key, str(value))
+    return zeroRemover(emojiIp)
 
-	return zeroRemover(emojiIp)
-
-#Make a command from a tweet: <.*>
-'''def commandMaker(tweet):
-	digitByDigit = list(tweet)
-	c = 0
-	while c < len(digitByDigit):
-		if (digitByDigit[c] == '<'):
-
-		else:
-			c += 1
-'''
-	
 
 # Get all trends from trends24.in for a specific country
 # using BeautifulSoup (bs4)
 #
 # Pick which table you want to work with at trends24
-trendsTime = 5 # specify the trend time in hours ( 0: current trends in this hour, 1: an hour ago, 2: two hours ago, and so on)
-countryName = 'netherlands' #keep it empty for worldwide or specify country. such as united-kingdom, netherlands, and so on
+# specify the trend time in hours ( 0: current trends in this hour, 1: an
+# hour ago, 2: two hours ago, and so on)
+trendsTime = 1
+# keep it empty for worldwide or specify country. such as united-kingdom,
+# netherlands, and so on
+countryName = 'netherlands'
 pageLink = 'https://trends24.in/' + countryName + '/'
 pageResponse = requests.get(pageLink, timeout=5)
 pageContent = BeautifulSoup(pageResponse.content, "html.parser")
 trendTag = pageContent.find_all(class_='trend-card__list')
 
-#A Regular expression like to remove HTML tags: <[^>]*>
-#print (trendTag[trendsTime].get_text(',', strip=True).encode('utf-8'))
+# A Regular expression like to remove HTML tags: <[^>]*>
+# print (trendTag[trendsTime].get_text(',', strip=True).encode('utf-8'))
 allTrends = (trendTag[trendsTime].get_text(',', strip=True)).split(',')
-#allUsableTrends = [x for x in allTrends if isEnglish(x)]
-hashtagTrends = [x for x in allTrends if "#" in x and isEnglish(x)] # get a clean list for only trends with hashtag and english language
+# allUsableTrends = [x for x in allTrends if isEnglish(x)]
+# get a clean list for only trends with hashtag and english language
+hashtagTrends = [x for x in allTrends if "#" in x and isEnglish(x)]
 
 print(hashtagTrends)
 
-#Search query that check for each of the three most hashtaged trends using
-searchQuery = hashtagTrends[0] + " OR " + hashtagTrends[1] + " OR " + hashtagTrends[2]
-#print(searchQuery)
+# Search query that check for each of the three most hashtaged trends using
+searchQuery = hashtagTrends[0] + " OR " + \
+    hashtagTrends[1] + " OR " + hashtagTrends[2]
+# print(searchQuery)
 
+# list of command to execute by substituting the <IP-Address> with the
+# actual IP:
+commandsList = {hashtagTrends[0]: 'echo \'The guy with this ip-address: <IP-Address> is a Legend! Just a Legend !!\' > legendQuoteofTheDay.txt',
+                hashtagTrends[1]: 'dig <IP-Address> > dig_for_<IP-Address>.txt',
+                hashtagTrends[2]: 'nslookup <IP-Address> > nslookup_for_<IP-Address>.txt'}
 
 # Get all tweets for a specific user (for a specific hashtags)
 c = twint.Config()
 c.Username = "delgado_ahmed"
-c.Search =  '#weekend'  #Seach for specific hashtag name. In this case it used the trending list to fetsh for that specific hashtag. you can also search for multiple hashtags using advance query
+c.Search = searchQuery  # Seach for specific hashtag name. In this case it used the trending list to fetsh for that specific hashtag. you can also search for multiple hashtags using advance query
 c.Format = "{tweet}"
-#c.Since = TODAY #Specify the tweet time (today)
+c.Since = TODAY  # Specify the tweet time (today)
 
-#c.Lang = "en" #specify tweet language 
+# c.Lang = "en" #specify tweet language
 c.Output = "tweets.txt"
 
 # Run
 twint.Search(c)
 
-#After output all the tweets to a file, I only take the last one
-#FIX ME to open only if i exists
-with open('tweets.txt') as f:
-    lastTweet = f.readline() #get the last tweet
-    os.remove('tweets.txt')
+# After output all the tweets to a file, I only take the last one
 
 
-print(lastTweet)
+def commander():
+    com = ' '
+    try:
+        with open('tweets.txt') as f:
+            lastTweet = f.readline()  # get only the last tweet
 
+        if (isCommand(lastTweet)):
+            for i in range(0, 3):
+                if hashtagTrends[i] in lastTweet:
+                    com = commandsList[hashtagTrends[i]]
+                    com = com.replace('<IP-Address>', toIP(lastTweet))
+        #os.remove('tweets.txt')
+    except IOError:
+        print("There is no Tweets for today yet")
 
+    return com
 
-print(toIP(z))
-#run commands on server
-#command = 'ping -c 4 '+ x + ' > ' + x + '.txt'
-#command = 'dig ' + x + ' > ' + x + '.txt'
-#command = 'nslookup ' + x + ' > ' + x + '.txt'
-#print(command)
-#os.system(command)
+def main():
+	if (commander() != ' '):
+		print(commander())
+		os.system(commander())
+	else:
+		print('No command has been called !!')
 
-
-
+main()
